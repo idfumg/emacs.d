72dad4035dcafce57adc2f451c3bf077dc4d082a
idfumg <idfumg@gmail.com>
2015-08-12 20:18:53 +0300

    git init

---
+++ irony/build/src/CMakeFiles/irony-server.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src/Command.cpp" "/home/idfumg/.emacs.d/irony/build/src/CMakeFiles/irony-server.dir/Command.cpp.o"
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src/Irony.cpp" "/home/idfumg/.emacs.d/irony/build/src/CMakeFiles/irony-server.dir/Irony.cpp.o"
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src/TUManager.cpp" "/home/idfumg/.emacs.d/irony/build/src/CMakeFiles/irony-server.dir/TUManager.cpp.o"
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src/main.cpp" "/home/idfumg/.emacs.d/irony/build/src/CMakeFiles/irony-server.dir/main.cpp.o"
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src/support/CommandLineParser.cpp" "/home/idfumg/.emacs.d/irony/build/src/CMakeFiles/irony-server.dir/support/CommandLineParser.cpp.o"
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src/support/TemporaryFile.cpp" "/home/idfumg/.emacs.d/irony/build/src/CMakeFiles/irony-server.dir/support/TemporaryFile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/idfumg/.emacs.d/elpa/irony-20141217.1417/server/src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
